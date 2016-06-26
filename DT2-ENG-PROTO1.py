ea2a51c42cec8f0bff96000159ad14586160ea40
thomasthespacefox <teleathers22@gmail.com>
2016-06-26 17:46:22 -0400

    add base image to splash images, add comments to code, revise some images

--- DT2-ENG-PROTO1.py
+++ DT2-ENG-PROTO1.py
@@ -27,15 +27,23 @@ pygame.display.set_caption("Doorway Trivia 2", "Doorway Trivia 2")
 simplefont = pygame.font.SysFont(None, 16)
 simplefontB = pygame.font.SysFont(None, 22)
 
+#load images
 bg1=pygame.image.load(os.path.join('TEX', 'bg1.png'))
 correctpic=pygame.image.load(os.path.join('TEX', 'DOOR_OPEN_HALL.PNG'))
 incorrectpic=pygame.image.load(os.path.join('TEX', 'DOOR_OPEN_FALL.PNG'))
+splashplate=pygame.image.load(os.path.join('TEX', 'splashplate.png'))
+#find centering coords of varoius splash images
 correctpicbox = correctpic.get_rect()
 correctpicbox.centerx = screensurf.get_rect().centerx
 correctpicbox.centery = ((screensurf.get_rect().centery))
+splashbox = splashplate.get_rect()
+splashbox.centerx = screensurf.get_rect().centerx
+splashbox.centery = ((screensurf.get_rect().centery))
 incorrectpicbox = incorrectpic.get_rect()
 incorrectpicbox.centerx = screensurf.get_rect().centerx
 incorrectpicbox.centery = ((screensurf.get_rect().centery))
+
+#imput function used in splash screen functions.
 
 def inpwait():
 	while True:
@@ -43,7 +51,7 @@ def inpwait():
 		for event in pygame.event.get():
 			if event.type == KEYDOWN:
 				return()
-
+#text functions used by splash screens
 def splashtext(texttodraw):
 	textit=simplefontB.render(texttodraw, True, (0, 0, 0), (255, 255, 255))
 	textbox = textit.get_rect()
@@ -59,24 +67,30 @@ def splashtextB(texttodraw):
 	textbox.centery = ((screensurf.get_rect().centery + 68))
 	screensurf.blit(textit, textbox)
 	pygame.display.update()
-
+#splash called upon correct awnser
 def correctsplash():
+	screensurf.blit(splashplate, splashbox)
 	screensurf.blit(correctpic, correctpicbox)
 	splashtext("Correct!")
 	pygame.display.update()
 	pygame.event.pump()
 	pygame.event.clear()
 	inpwait()
+#splash called upon incorrect awnser
 def incorrectsplash():
+	screensurf.blit(splashplate, splashbox)
 	screensurf.blit(incorrectpic, incorrectpicbox)
 	splashtext("Incorrect!")
 	pygame.display.update()
 	pygame.event.pump()
 	pygame.event.clear()
 	inpwait()
+#splash called upon script completion
 def winsplash():
+	screensurf.blit(splashplate, splashbox)
 	#screensurf.blit(correctpic, correctpicbox)
 	splashtext("Final Score:")
+	#shows final values of score and scoreof, thus showing the player their score. 
 	splashtextB((str(score) + "/" + str(scoreof)))
 	pygame.display.update()
 	pygame.event.pump()
@@ -84,10 +98,12 @@ def winsplash():
 	inpwait()
 
 screensurf.blit(bg1, (0, 0))
+#function used to get lines from trivia script
 def getinst(linenum):
 	linet=str(linecache.getline(trivfilepath, linenum)).replace("\n", "")
 	#print ("getinst:\n" + linet)
 	return(linet)
+#some predefining of variables
 linetxt=""
 lineno=1
 trivauthor="unspecified"
@@ -96,7 +112,9 @@ trivtitle="unnamed"
 score=0
 scoreof=0
 
-
+#function used when a list should be shown as an interactive menu. i.e. multiple choice (:multi) questions.
+#displays the list as interactive menu and returns the number in the list that is selected by the player
+#(top/first entry would return as 1 if selected) 
 def iteratelist(listtoiterate):
 	findcnt=0
 	for flx in listtoiterate:
@@ -143,7 +161,7 @@ def iteratelist(listtoiterate):
 					evhappenflg=1
 					return(listhighnum)
 		
-
+#draws teh question text and shows the question number.
 def drawqtext(texttodraw):
 	textchunk=""
 	#print ("receved:" + texttodraw)
@@ -165,7 +183,7 @@ def drawqtext(texttodraw):
 			textchunk=(textchunk + texch)
 			#print "pong"
 	pygame.display.update()
-
+#main loop
 while linetxt!=":end":
 	screensurf.blit(bg1, (0, 0))
 	#time.sleep(0.8)
@@ -173,9 +191,10 @@ while linetxt!=":end":
 	#print("mainloop")
 	sublinetxt=""
 	sublinetxtb=""
+	#various declare functions
 	if linetxt==':declare-title':
 		trivtitle=getinst(lineno + 1)
-		pygame.display.set_caption(("Doorway Trivia 2 |" + trivtitle), ("Doorway Trivia 2 |" + trivtitle))
+		pygame.display.set_caption(("Doorway Trivia 2 | " + trivtitle), ("Doorway Trivia 2 | " + trivtitle))
 		print("set title to:" + trivtitle)
 		lineno += 2
 	elif linetxt==':declare-author':
@@ -186,6 +205,7 @@ while linetxt!=":end":
 		trivmode=getinst((lineno + 1))
 		print("set mode to:" + trivmode)
 		lineno += 2
+	#interpeter for multiple choice questions
 	elif linetxt==":multi":
 		while linetxt!="::":
 			if linetxt==":qtext":
@@ -226,8 +246,11 @@ while linetxt!=":end":
 			scoreof +=1
 			print("incorrect")
 			incorrectsplash()
+	#called when a function is not reconized, does not raise an exception when 
+	#this occures, such as a comment i.e. (#this a a comment)
 	else:
 		print("unkown" + linetxt)
 		lineno += 1
+#show winsplash
 winsplash()
 print "end"
